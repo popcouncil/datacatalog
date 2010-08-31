2b1c743b5b4b767122777546322d2dccf714922a
Gabriela Rodríguez and Nicolás Sanguinetti <gaba+foca@cuboxsa.com>
2010-08-31 16:36:33 -0300

    Default to ENV vars for the API config if the file isn't present

--- config/initializers/datacatalog.rb
+++ config/initializers/datacatalog.rb
@@ -1,16 +1,20 @@
 # Be sure to restart your server when you modify this file.
 
-file = File.expand_path(File.dirname(__FILE__) + "/../api.yml")
-api_config = YAML.load_file(file)
-api_config_env = api_config[RAILS_ENV]
-unless api_config_env
-  raise %(Environment "#{RAILS_ENV}" not found in #{file})
+api_file = Rails.root.join("config/api.yml")
+if api_file.file?
+  api_config = YAML.load_file(api_file)
+  api_config_env = api_config.fetch(Rails.env)
+else
+  api_config_env = {
+    "api_key"  => ENV["API_KEY"],
+    "base_uri" => ENV["API_BASE_URI"]
+  }
 end
-unless api_config_env['api_key']
+
+DataCatalog.api_key  = api_config_env.fetch('api_key') {
   raise %(api_key not found for environment "#{RAILS_ENV}" in #{file})
-end
-unless api_config_env['base_uri']
+}
+
+DataCatalog.base_uri = api_config_env.fetch('base_uri') {
   raise %(base_uri not found for environment "#{RAILS_ENV}" in #{file})
-end
-DataCatalog.api_key  = api_config_env['api_key']
-DataCatalog.base_uri = api_config_env['base_uri']
+}
