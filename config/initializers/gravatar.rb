58d43a4b6c0e34a2daa2dd445afac809c0412826
Gabriela Rodríguez and Nicolás Sanguinetti <gaba+foca@cuboxsa.com>
2010-09-02 17:56:10 -0300

    Fix the hardcoded strings in the controller ¬¬

--- config/initializers/gravatar.rb
+++ config/initializers/gravatar.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-ActionView::Base.default_gravatar_image  = "http://nationaldatacatalog.com/images/avatar_female_lg.png"
+ActionView::Base.default_gravatar_image  = "/images/avatar_female_lg.png"
 ActionView::Base.default_gravatar_size  = 64
