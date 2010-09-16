4135bd1b837eb08670fefdf645d03638337095ad
Gabriela Rodríguez and Nicolás Sanguinetti <gaba+foca@cuboxsa.com>
2010-09-16 15:29:07 -0300

    Don't use a config file for S3, use the ENV

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,10 @@
 if Rails.env.production?
   PAPERCLIP_CONFIG = {
     :storage        => :s3,
-    :s3_credentials => Rails.root.join("config/s3.yml"),
+    :s3_credentials => {
+      :access_key_id     => ENV["S3_ACCESS_KEY"],
+      :secret_access_key => ENV["S3_SECRET_KEY"]
+    }
     :bucket         => "threed-staging",
     :path           => ":attachment/:id/:style.:extension"
   }
