032d3fe3ed0bdb3a66115525c3fc48e5f36debdd
Gabriela Rodríguez and Nicolás Sanguinetti <gaba+foca@cuboxsa.com>
2010-09-16 15:30:54 -0300

    Oops, silly rabbit

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
     :s3_credentials => {
       :access_key_id     => ENV["S3_ACCESS_KEY"],
       :secret_access_key => ENV["S3_SECRET_KEY"]
-    }
+    },
     :bucket         => "threed-staging",
     :path           => ":attachment/:id/:style.:extension"
   }
