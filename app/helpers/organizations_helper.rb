228556c9f69b638cc8b3d44e2a0ca1fc7b5d2bda
Gabriela Rodríguez and Nicolás Sanguinetti <gaba+foca@cuboxsa.com>
2010-10-07 17:47:43 -0200

    Show all organizations on the Browse table

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -12,4 +12,16 @@ module OrganizationsHelper
       [name, id.to_s]
     end
   end
+
+  def organizations_for(data_record)
+    data_record.sponsors.map do |sponsor|
+      if sponsor.lead?
+        [content_tag(:strong, sponsor.organization.name), sponsor.organization]
+      else
+        [sponsor.organization.name, sponsor.organization]
+      end
+    end.map do |name, org|
+      link_to name, org
+    end.join(", ")
+  end
 end
