c0ee64d1a9d03836bcb14cdd8893844c34abacc6
Gabriela Rodríguez and Nicolás Sanguinetti <gaba+foca@cuboxsa.com>
2010-10-12 18:21:56 -0200

    Changed users.name to user.display_name to sort by ministry in the browsing table.

--- lib/browse_table_sorts.rb
+++ lib/browse_table_sorts.rb
@@ -5,7 +5,7 @@ module BrowseTableSorts
       sortable_attributes :"lower(data_records.title)", :title        => "lower(data_records.title)",
                                                         :rating       => "CASE(data_records.ratings_count)WHEN(0)THEN(0)ELSE(ROUND(data_records.ratings_total/data_records.ratings_count))END",
                                                         :location     => "lower(locations.name)",
-                                                        :ministry     => "lower(users.name)",
+                                                        :ministry     => "lower(users.display_name)",
                                                         :organization => "lower(organizations.name)",
                                                         :format       => "lower(documents.document_type)",
                                                         :tags         => "lower(tags.name)"
